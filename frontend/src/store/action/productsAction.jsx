ee2b02471dae3d6c65333f9b1ae62ad839b8bcf2
Abhay Bhardwaj <86096489+iamabhay17@users.noreply.github.com>
2024-08-12 02:13:16 +0530

    Update productsAction.jsx

--- frontend/src/store/action/productsAction.jsx
+++ frontend/src/store/action/productsAction.jsx
@@ -4,7 +4,7 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: "PRODUCT_LIST_REQUEST" });
     const { data } = await axios.get(
-      "https://shopper-wmd5.onrender.com/api/products"
+      "https://shopper-api.vercel.app/api/products"
     );
 
     dispatch({ type: "PRODUCT_LIST_SUCCESS", payload: data });
@@ -25,7 +25,7 @@ export const listProductDetail = (id) => async (dispatch) => {
   try {
     dispatch({ type: "PRODUCT_DETAILS_REQUEST" });
     const { data } = await axios.get(
-      `https://shopper-wmd5.onrender.com/api/products/${id}`
+      `https://shopper-api.vercel.app/api/products/${id}`
     );
 
     dispatch({ type: "PRODUCT_DETAILS_SUCCESS", payload: data });
@@ -57,7 +57,7 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     };
 
     await axios.delete(
-      `https://shopper-wmd5.onrender.com/api/products/${id}`,
+      `https://shopper-api.vercel.app/api/products/${id}`,
       config
     );
 
@@ -90,7 +90,7 @@ export const createProduct = () => async (dispatch, getState) => {
     };
 
     const { data } = await axios.post(
-      `https://shopper-wmd5.onrender.com/api/products/`,
+      `https://shopper-api.vercel.app/api/products/`,
       {},
       config
     );
@@ -124,7 +124,7 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     };
 
     const { data } = await axios.put(
-      `https://shopper-wmd5.onrender.com/api/products/${product._id}`,
+      `https://shopper-api.vercel.app/api/products/${product._id}`,
       product,
       config
     );
